c0d5738181ef05f4c736824414530331759a01a3
Leandro Barbosa <leandrobarbosa@sfr.fr>
2025-07-01 01:32:39 +0200

    cleanup about page

--- nextjs-uk-events/src/app/components/About.tsx
+++ nextjs-uk-events/src/app/components/About.tsx
@@ -43,17 +43,21 @@ export default function About() {
 
   return (
     <section className="right-image-section-about min-h-screen">
-      <div className="gap-8 grid grid-cols-12 px-4">
-        <h1 className="row-start-1 col-span-12 text-4xl sm:text-7xl text-center text-[#33483e] font-bold">
+      <div className="grid grid-cols-12 gap-8 px-4">
+        <h1 className="col-span-12 text-4xl sm:text-7xl text-center text-[#33483e] font-bold">
           {aboutData.title}
         </h1>
         <h3 className="col-span-12 text-[#33483e] font-bold text-center text-xl sm:text-4xl">
           {aboutData.subtitle}
         </h3>
-        <div className="col-span-12 lg:col-start-7 lg:col-end-13 text-[#33483e] text-lg item-description">
+
+        {/* Texte entre colonnes 2 et 7 */}
+        <div className="col-span-12 lg:col-start-2 lg:col-end-7 text-[#33483e] text-lg item-description">
           <PortableText value={aboutData.body} />
         </div>
-        <div className="col-span-12 lg:col-span-5">
+
+        {/* Image entre colonnes 7 et 11 */}
+        <div className="col-span-12 lg:col-start-7 lg:col-end-11 flex justify-center">
           {aboutData.image && (
             <div className="about-image-wrapper">
               <Image
