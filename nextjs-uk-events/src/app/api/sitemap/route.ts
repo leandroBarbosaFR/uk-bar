af5e86e0769fa80c6500c49074d2e9fc68cec16e
Leandro Barbosa <leandrobarbosa@sfr.fr>
2025-07-01 10:57:58 +0200

    fixed route xml

--- nextjs-uk-events/src/app/api/sitemap/route.ts
+++ nextjs-uk-events/src/app/api/sitemap/route.ts
@@ -2,20 +2,19 @@ import {NextResponse} from 'next/server'
 
 export async function GET() {
   const staticPages = [
-    '',
-    'contact',
-    'terms-and-conditions',
-    'privacy-policy',
-    'mobile-bar-packages',
-    'master-class',
-    'entertainment',
-    'corporate-bar-hire',
-    'cocktail-bar-hire',
-    'about',
-    '/',
+    '/', // only this is needed for home page
+    '/contact',
+    '/terms-and-conditions',
+    '/privacy-policy',
+    '/mobile-bar-packages',
+    '/master-class',
+    '/entertainment',
+    '/corporate-bar-hire',
+    '/cocktail-bar-hire',
+    '/about',
   ]
 
-  const baseUrl = 'https://www.sambabarevents.co.uk/'
+  const baseUrl = 'https://www.sambabarevents.co.uk'
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -23,14 +22,15 @@ export async function GET() {
     .map(
       (page) => `
   <url>
-    <loc>${baseUrl}/${page}</loc>
-  </url>
-  `,
+    <loc>${baseUrl}${page}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>${page === '/' ? '1.0' : '0.8'}</priority>
+  </url>`,
     )
-    .join('')}
+    .join('\n')}
 </urlset>`
 
-  return new NextResponse(sitemap, {
+  return new NextResponse(sitemap.trim(), {
     headers: {
       'Content-Type': 'application/xml',
     },
